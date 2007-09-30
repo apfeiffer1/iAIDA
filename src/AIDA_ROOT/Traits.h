9169251bcef4e0f54483efff3be62cae0bb41e6c
ap <ap@252513f7-a235-0410-ab3d-677bfe8fc1a3>
2007-09-30 16:14:25 +0000

    added LGPL (v3) license

--- src/AIDA_ROOT/Traits.h
+++ src/AIDA_ROOT/Traits.h
@@ -1,3 +1,23 @@
+//
+//    Copyright 2007 Andreas Pfeiffer
+//
+/*
+ *  This file is part of iAIDA.
+ *
+ *  iAIDA is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as 
+ *  published by the Free Software Foundation, either version 3 of 
+ *  the License.
+ *
+ *  iAIDA is distributed in the hope that it will be useful, but 
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public 
+ *  License along with iAIDA.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef AIDAROOT_Traits_H
 #define AIDAROOT_Traits_H 1
 #include "AIDA_ROOT/Histogram1D.h"
