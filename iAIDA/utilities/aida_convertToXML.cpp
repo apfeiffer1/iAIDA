33cbc115f76acdd83628a7d9045cd138d5deedd2
Andreas Pfeiffer <andreas.pfeiffer@cern.ch>
2013-10-11 22:04:39 +0200

    use cp() method from tree to copy.

--- iAIDA/utilities/aida_convertToXML.cpp
+++ iAIDA/utilities/aida_convertToXML.cpp
@@ -8,146 +8,180 @@
 #include <AIDA/IManagedObject.h>
 #include <AIDA/ITuple.h>
 #include <iostream>
+#include <fstream>
 
-bool process_file(AIDA::IAnalysisFactory& aAIDA,
-                  const std::string& a_file,
-                  const std::string& a_format,
-                  const std::string& a_path,
-                  std::ostream& a_out) {
+// #define NDEBUG 
 
-  AIDA::ITreeFactory* treeFactory = aAIDA.createTreeFactory();
-  if(!treeFactory) {
-    std::cout << "process_file : can't get a TreeFactory." << std::endl;
-    return false;
-  }
-  AIDA::ITree* tree = treeFactory->create(a_file,a_format,true,false);
-  delete treeFactory;
-  if(!tree) {
-    std::cout << "process_file : can't open data file." << std::endl;
-    return false;
-  }
-  std::string dir;
-  std::string name;
- {std::string::size_type pos = a_path.rfind('/');
-  if(pos==std::string::npos) {
-    name = a_path;
-  } else {
-    dir = a_path.substr(0,pos);pos++;
-    name = a_path.substr(pos,a_path.size()-pos);  
-  }}
-  if(dir.size()){
-    if(!tree->cd(dir)) {
-      std::cout << "process_file :"
-                << " can't cd to " << dir << "."
-                << std::endl;
-      delete tree;
+std::string findFileType(std::string fileName) {
+
+   typedef unsigned int uint;
+
+   char buf[5];
+
+   std::ifstream is;
+   is.open (fileName.c_str(), std::ios::in | std::ios::binary );
+   is.read ( buf, 4 ) ; // read first 4 bytes
+   is.close();
+   buf[4] = '\0'; // "close" string, just in case ...
+
+   // values from /usr/share/file/magic/compress on Mac OS X
+   std::string result = "None";
+   if ( (char(buf[0]&0xff) == '<' ) && ( char(buf[1]&0xff) == '?' ) ) {  // xml
+      result = "uncompress";
+   } else if ( ( uint(buf[0]&0xff) == 0x1f ) && 
+               ( uint(buf[1]&0xff) == 0x9d ) ) {  // compress'd 
+      result = "zlib";                        
+   } else if ( ( uint(buf[0]&0xff) == 0x1f ) && 
+               ( uint(buf[1]&0xff) == 0x8b ) ) {  // gzip'd 
+      result = "gzip";
+   } else if ( ( char(buf[0]&0xff) == 'B' ) && 
+               ( char(buf[1]&0xff) == 'Z' ) && 
+               ( char(buf[2]&0xff) == 'h' ) ) {  // bzip2'd ("BZh")
+      result = "bzip";
+   } else {
+      // assume zlib compression (needs to uncompress to determine if things are ok ... :( )
+      result = "zlib";
+   }
+   return result;
+}
+
+
+bool process_file(AIDA::IAnalysisFactory& aAIDA, 
+                  const std::string& inFile, 
+                  const std::string& inType, 
+                  const std::string& outFile) {
+
+   AIDA::ITreeFactory* treeFactory = aAIDA.createTreeFactory();
+   if(!treeFactory) {
+      std::cout << "process_file : can't get a TreeFactory." << std::endl;
       return false;
-    }
-  }
-  AIDA::IManagedObject* object = tree->find(name);
-  if(!object) {
-    std::cout << "process_file : "
-              << " object " << name << " not found in tree." 
-              << std::endl;
-    delete tree;
-    return false;
-  }
-  AIDA::ITuple* tuple = dynamic_cast<AIDA::ITuple*>(object);
-  if(!tuple) {
-    std::cout << "process_file : object not an AIDA::ITuple." << std::endl;
-    delete tree;
-    return false;
-  }
-  //bool verbose = false;
-  bool verbose = true;
-  if(verbose) {
-    int coln = tuple->columns();
-    for(int index=0;index<coln;index++) {
-      std::cout << "process_file :"
-                << " icol = " << index
-                << ", label = " << tuple->columnName(index) 
-                << ", type = " << tuple->columnType(index) 
-                << std::endl;
-    }
-    std::cout << "process_file : rows = " << tuple->rows() << std::endl;
-  }
-  int coln = tuple->columns();
-  std::vector<std::string> types = tuple->columnTypes();
-  char sep = ',';
-  tuple->start();
-  while(tuple->next()) {
-    for(int col=0;col<coln;col++) {
-      if(col) a_out << sep;
-      if(types[col]=="float") {
-        float v = tuple->getFloat(col);a_out << v;
-      } else if(types[col]=="double") {
-        double v = tuple->getDouble(col);a_out << v;
-      } else if(types[col]=="char") {
-        char v = tuple->getChar(col);a_out << v;
-      } else if(types[col]=="short") {
-        short v = tuple->getShort(col);a_out << v;
-      } else if(types[col]=="int") {
-        int v = tuple->getInt(col);a_out << v;
-      } else if(types[col]=="long") {
-        long v = tuple->getLong(col);a_out << v;
-      } else if(types[col]=="boolean") {
-        bool v = tuple->getBoolean(col);a_out << v;
-      } else if(types[col]=="string") {
-	std::string v = tuple->getString(col);a_out << v;
-      } else {
-        std::cout << "process_file : unknown type." << types[col] << std::endl;
-        delete tree;
-        return false;
-      }
-    }
-    a_out << std::endl;
-  }
-  delete tree;
-  return true;
+   }
+   
+   // first create an in-memory tree to act as a common handle.
+   // we then mount the input and output trees and use the cp() method to get the objects copied over
+
+   AIDA::ITree* memTree = treeFactory->create();
+   if( !memTree ) {
+      std::cout << "process_file : can't create in-memory tree !" << std::endl;
+      return false;
+   }
+   
+   // open input file readonly, do not create new one if it doesn't exist. 
+   // check type of file if we have a xml file derivative
+   std::string options("");
+   if ( inType == "xml" ) {
+      options = findFileType(inFile);
+      std::cout << "found file " << inFile << " to be of type " << options << std::endl;
+   }
+   AIDA::ITree* inTree = treeFactory->create(inFile, inType, true, false, options);
+   if( ! inTree ) {
+      std::cout << "process_file : can't open input file " << inFile << std::endl;
+      return false;
+   }
+   
+   // open output file in read/write, create new one if it doesn't exist
+   options = "uncompress"; // use uncompressed xml for output
+   AIDA::ITree* outTree = treeFactory->create(outFile, "xml", false, true, options);
+   if( !outTree ) {
+      std::cout << "process_file : can't open output file " << outFile << std::endl;
+      return false;
+   }
+
+   bool recursive = true;
+
+   memTree->mkdir("/in");
+   memTree->mkdir("/out");
+   
+   if ( ! memTree->mount("/in" , *inTree , "/") ) std::cout << "Error mounting inTree" << std::endl;
+   if ( ! memTree->mount("/out", *outTree, "/") ) std::cout << "Error mounting outTree" << std::endl;
+
+#ifdef NDEBUG
+   std::cout << "\n inTree :" << std::endl;
+   inTree->ls("/", recursive);
+
+   std::cout << "\n memTree (before):" << std::endl;
+   memTree->ls("/", recursive);
+#endif
+
+   if ( ! memTree->cp("/in", "/out", recursive) ) std::cout << "Error copying /in to /out" << std::endl;
+
+#ifdef NDEBUG
+   std::cout << "\n memTree (after):" << std::endl;
+   memTree->ls("/", recursive);
+#endif
+
+   if ( ! memTree->unmount("/in")  ) std::cout << "Error unmounting /in " << std::endl;
+   if ( ! memTree->unmount("/out") ) std::cout << "Error unmounting /out" << std::endl;
+
+#ifdef NDEBUG
+   std::cout << "\n outTree :" << std::endl;
+   outTree->ls("/", recursive);
+#endif
+
+   if ( ! outTree->commit() ) std::cout << "Error committing /out" << std::endl;
+   if ( ! outTree->close()  ) std::cout << "Error closing /out" << std::endl;
+
+   inTree->close();
+
+   memTree->close();
+
+   // delete outTree;
+   delete inTree;
+   delete memTree;
+
+   delete treeFactory; // not needed any more
+
+   std::cout << std::endl;
+
+   return true;
 }
 //////////////////////////////////////////////////////////////////////////////
 
 #include <fstream>
 
-int main(int argc,char* argv[]) {
+   int main(int argc,char* argv[]) {
 
-  std::string file("../examples/exatup.aida");
-  std::string format("xml");
-  std::string tuple("100");          
-  if(argc!=4) {
-      std::cout << "main : three arguments expected :"
-              << " <file>, <format>, <tuple path>"
-              << " -- but none given. Will assume: '" << file << " "<< format << " " << tuple <<"' "
-              << std::endl;
-  } else { // override defaults from args
-      file   = std::string(argv[1]);
-      format = std::string(argv[2]);
-      tuple  = std::string(argv[3]);
-  }
-  AIDA::IAnalysisFactory* aida = AIDA_createAnalysisFactory();
-  if(!aida) {
-    std::cout << "main : AIDA not found." << std::endl;
-    return 1;
-  }
-  std::ofstream out("out.csv");
-  if(out.fail()) {
-    std::cout << "main : can't open out.csv." << std::endl;
-    return 1;
-  }
+      std::string prgName = std::string(argv[0]);
 
-  bool result = process_file(*aida,file,format,tuple,out);
-  if(!result){
-    std::cout << "main :"
-              << " read_file " << file << " failed."
-              << std::endl;
-  }
-  out.close();
-  delete aida;
+      std::string inFile("../exatup.aida");
+      std::string inType("xml");
+      std::string outFile("../exatup.xml");
+      if ( argc != 4 ) {
+         std::cout << prgName << ": three arguments expected : "
+            << "<inputFile> <inputType> <outputFile> "
+            << "-- but none given. Will assume: '" << inFile << " " << inType << " " << outFile <<"' "
+            << std::endl;
+      } else { // override defaults from args
+         inFile  = std::string(argv[1]);
+         inType  = std::string(argv[2]);
+         outFile = std::string(argv[3]);
+      }
 
-  if (!result) {
-      return -1;
-  }
+      AIDA::IAnalysisFactory* aida = AIDA_createAnalysisFactory();
+      if(!aida) {
+         std::cout << prgName << ": AIDA not found." << std::endl;
+         return 1;
+      }
 
-  std::cout << "That's it !" << std::endl;
-  return 0;
-}
+      bool result = process_file(*aida, inFile, inType, outFile);
+      // clean up 
+      delete aida;
+
+      if(!result){
+         std::cout << prgName << ": "
+            << "converting file " << inFile << " to " << outFile << " failed."
+            << std::endl;
+         return -1;
+      } else {
+         std::cout << prgName << ": "
+            << "file " << inFile << " to " << outFile << " converted successfully."
+            << std::endl;
+      }
+
+
+      if (!result) {
+      }
+
+      std::cout << "That's it !" << std::endl;
+      return 0;
+   }
